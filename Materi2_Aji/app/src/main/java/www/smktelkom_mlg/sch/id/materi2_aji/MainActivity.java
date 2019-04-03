d4ad01a5d383cdbc19e4af7d27b8615e26fbe36d
maulanapranasutra <maulana_pranasutra_26rpl@student.smktelkom-mlg.sch.id>
2019-04-03 15:38:42 +0700

    selesai

---
+++ Materi2_Aji/app/src/main/java/www/smktelkom_mlg/sch/id/materi2_aji/MainActivity.java
@@ -0,0 +1,13 @@
+package www.smktelkom_mlg.sch.id.materi2_aji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
